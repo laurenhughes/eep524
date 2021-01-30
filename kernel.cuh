92b29e8553813313af97c15fd90b71e377622198
Lauren Hughes <lahugh@microsoft.com>
2021-01-30 20:19:19 +0000

    first commit

---
+++ kernel.cuh
@@ -0,0 +1,12 @@
+﻿#include "device_launch_parameters.h"
+
+__global__
+
+void vecadd_Id(float* result, float* a, float* b, int N) {
+	int index = threadIdx.x + blockIdx.x * blockDim.x;
+	int stride = blockDim.x * gridDim.x;
+
+	for (int i = index; i < N; i += stride) {
+		result[i] = a[i] + b[i];
+	}
+}
